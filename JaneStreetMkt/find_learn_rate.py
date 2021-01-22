4e126d68ff00d116e451a21d622d165c1fa81201
pspinaunipi <75396036+pspinaunipi@users.noreply.github.com>
2021-01-22 18:28:10 +0100

    stuff

--- JaneStreetMkt/find_learn_rate.py
+++ JaneStreetMkt/find_learn_rate.py
@@ -10,13 +10,14 @@ The best learning rate for our model is lr/10
 """
 
 import time
-import math
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_score, TimeSeriesSplit, train_test_split
-import matplotlib.pyplot as plt
+
 from keras.utils import np_utils
 from keras.layers import Input, Dense, Dropout, Activation, BatchNormalization
 from keras.models import Model
@@ -75,81 +76,128 @@ def build(data, input_dim, hidden_units, num_labels, dropout_rates, learning_rat
     return model
 
 
+def filter_loss(loss, beta):
+    """
+    this function computes the smoothed loss
+
+    Parameters:
+    ---------
+    loss: np.array of float
+        validation loss we want to smooth
+
+    beta: float
+        this paramer set how much strong the smoothing process is
+
+    Yields
+    ------
+    smoothed_loss: np.array of float
+        smoothed loss
+
+    """
+    num = len(loss)
+    # create two empty arrays
+    avg_loss = np.zeros(num)
+    smoothed_loss = np.zeros(num)
+
+    # compute avg loss
+    for i in range(num):
+        if i == 0:
+            avg_loss[0] = (1-beta)*loss[0]
+        else:
+            avg_loss[i] = beta*avg_loss[i-1]+(1-beta)*loss[i]
+
+            # compute smoothed loss
+            smoothed_loss[i] = avg_loss[i]/(1-beta**(i+1))
+
+    return smoothed_loss
+
+
 if __name__ == '__main__':
-    # import data
-    data = import_training_set()
-    # delete first 85 days of dataset
-    data = data[data["date"] > 85]
-    data["date"] = data["date"]-85
 
-    # Remove feature based on correlation
-    useless = feature_selection.main(0.93)
-    data = data.drop(useless, axis=1)
+    PLOT = True  # set to true if we want to plot
+    SEARCH = False  # set to true if we want to search best learning rates
 
-    # remove features based on MDI feature importance
-    redunant_feat = np.loadtxt("../FeatureSelection/Results/deleted_feat_skip85.csv", dtype="str")
-    data = data.drop(redunant_feat, axis=1)
+    if SEARCH is True:
+        # import data
+        data = import_training_set()
+        # delete first 85 days of dataset
+        data = data[data["date"] > 85]
+        data["date"] = data["date"]-85
 
-    print('Imported successfully')
-    features = [c for c in data.columns if 'feature' in c]
-    cv = TimeSeriesSplit(n_splits=5, gap=10)
+        # Remove feature based on correlation
+        useless = feature_selection.main(0.93)
+        data = data.drop(useless, axis=1)
 
-    # split data into train and test set
-    X_train, y_train, X_test, y_test = split_data(data)
+        # remove features based on MDI feature importance
+        redunant_feat = np.loadtxt(
+            "../FeatureSelection/Results/deleted_feat_skip85.csv", dtype="str")
+        data = data.drop(redunant_feat, axis=1)
 
-    # compute
-    input_dim = X_train.shape[1]
+        print('Imported successfully')
+        features = [c for c in data.columns if 'feature' in c]
+        cv = TimeSeriesSplit(n_splits=5, gap=10)
 
-    # create two empty arrays that will contain the learning rate and the loss values
-    # for each iteration
-    array_learn = []
-    value_loss = []
+        # split data into train and test set
+        X_train, y_train, X_test, y_test = split_data(data)
 
-    log_lr = []
-    avg_loss = 0
-    beta= 0.98 #smoothing parameter
-    # starting learning rate
-    learn_rate = 1e-8
+        # compute
+        input_dim = X_train.shape[1]
 
-    # computational time
-    start = time.time()
+        # create two empty arrays that will contain the learning rate and the loss values
+        # for each iteration
+        array_learn = np.zeros(40)
+        value_loss = np.zeros(40)
 
-    for i in range(100):
+        # starting learning rate
+        learn_rate = 1e-8 * 1.22**51
 
-        # set hyperparameters
-        hidden_units = [128, 256, 256, 128]
-        dropout_rates = [0.10143786981358652, 0.19720339053599725,
-                         0.2703017847244654, 0.23148340929571917, 0.2357768967777311]
+        # computational time
+        start = time.time()
+        for i in range(40):
 
-        # build model
-        print('Building model...')
-        model = build(data, input_dim, hidden_units, 1, dropout_rates, learn_rate)
-        print('Training model...')
+            # set hyperparameters
+            hidden_units = [128, 256, 256, 128]
+            dropout_rates = [0.10143786981358652, 0.19720339053599725,
+                             0.2703017847244654, 0.23148340929571917, 0.2357768967777311]
 
-        # fit the model
-        history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
-                            epochs=1, batch_size=8192, verbose=1)
+            # build model
+            print('Building model...')
+            model = build(data, input_dim, hidden_units, 1, dropout_rates, learn_rate)
+            print('Training model...')
 
-        print(history.history.keys())
+            # fit the model
+            history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
+                                epochs=1, batch_size=8192, verbose=1)
 
-        #compute the average loss for each step
-        avg_loss = beta*avg_loss[i]+(1-beta)*value_loss[0]
-        smoothed_loss=avg_loss/(1-beta**(i+1))
-        if i>1 and smoothed_loss[i]>(4*best_loss):
-            return log_lr,value_losses
-        if smoothed_loss<4*best_loss or i==1:
-            best_loss=smoothed_loss
+            print(history.history.keys())
 
-        #store losses and learning rates
-        value_loss.append((history.history['val_loss'][0]))
-        array_learn.append(math.log10(learn_rate))
-        # set learning rate for next cycle
-        learn_rate = learn_rate*1.22
+            # save values of loss and learning rate
+            value_loss[i] = (history.history['loss'][0])
+            array_learn[i] = learn_rate
 
-    # save results t a csv file so we can use them later
-    np.savetxt("loss1.csv", value_loss, delimiter=',')
-    np.savetxt("learning_rate1.csv", array_learn, delimiter=',')
+            # set learning rate for next cycle
+            learn_rate = learn_rate*1.22
+            print(learn_rate)
 
-    plt.plot(array_learn, value_loss)
-    plt.xscale("log")
-    plt.show()
+        # save results t a csv file so we can use them later
+        np.savetxt("losss.txt", value_loss)
+
+    if PLOT is True:
+        raw_loss = np.loadtxt("losss.txt")
+        new_loss = filter_loss(raw_loss, 0.95)
+
+        l_rate = np.zeros(39)
+        for i in range(39):
+            l_rate[i] = 1.22**(51+i)*1e-8
+
+        new_loss[0] = 0.7559  # i don't know why but it doesn't register first value
+
+        # plot
+        plt.plot(l_rate, new_loss, label="Smoothed loss")
+        plt.plot(l_rate, raw_loss, label="Raw loss")
+        plt.xlabel("Learning rate")
+        plt.ylabel("Loss")
+        plt.title("Best loss over 1 epoch training", fontsize=18)
+        plt.legend()
+        plt.xscale("log")
+        plt.show()
