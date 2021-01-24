7d7078844c561e2372bc1fd71ae9a2b5d4d42fda
pspinaunipi <75396036+pspinaunipi@users.noreply.github.com>
2021-01-24 10:33:37 +0100

    stuff p3

--- JaneStreetMkt/find_learn_rate.py
+++ JaneStreetMkt/find_learn_rate.py
@@ -5,39 +5,32 @@ In the first part of the model we build 100 models with a linearly encreasing
 learning rate over a period of only one epoch.
 Then, after applying a smoothing algorithm, we find the learning rate lr which
 minimizes the loss.
-The best learning rate for our model is lr/10
-
+The best learning rate for our model is lr/10.
+The reason we use this tecnique rather than getting the learning rate through an
+hyperparameter search, is that  we need to fit the model over only one epoch
+thus reducing by far the time required to find the best learning rate.
 """
 
 import time
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_val_score, TimeSeriesSplit, train_test_split
-
-from keras.utils import np_utils
 from keras.layers import Input, Dense, Dropout, Activation, BatchNormalization
 from keras.models import Model
 import keras.losses
+import feature_selection
 from initial_import import import_training_set
 from splitting import split_data
-import feature_selection
 
 
-def build(data, input_dim, hidden_units, num_labels, dropout_rates, learning_rate):
+def build(input_dim, hidden_units, num_labels, dropout_rates, learning_rate):
     """
     This function builts the deep neural network used for the training.
 
     Parameters
     ----------
-    data: pd.DataFrame
-        competition dataset.
 
     input_dim: int
-        number of rows of competiton dataset
+        number of rows of competiton data
 
     hidden_units: list of 4 int
         number of hidden units for each layer
@@ -54,21 +47,21 @@ def build(data, input_dim, hidden_units, num_labels, dropout_rates, learning_rat
     Yields
     ------
     model:
-        model?
+        the neural network model
     """
 
-    input = Input(shape=(input_dim, ))
-    x = BatchNormalization()(input)  # re-centring and rescaling input layer
+    input_layer = Input(shape=(input_dim, ))
+    layer = BatchNormalization()(input_layer)  # re-centring and rescaling input layer
     # a fraction of nodes is discarded with a frequency equal to the rate
-    x = Dropout(dropout_rates[0])(x)
-    for i in range(len(hidden_units)):
-        x = Dense(hidden_units[i])(x)
-        x = BatchNormalization()(x)
-        x = Activation('relu')(x)
-        x = Dropout(dropout_rates[i+1])(x)
+    layer = Dropout(dropout_rates[0])(layer)
+    for i, unit in enumerate(hidden_units):
+        layer = Dense(unit)(layer)
+        layer = BatchNormalization()(layer)
+        layer = Activation('relu')(layer)
+        layer = Dropout(dropout_rates[i+1])(layer)
 
-    output = Dense(num_labels, activation='sigmoid')(x)
-    model = Model(inputs=input, outputs=output)
+    output = Dense(num_labels, activation='sigmoid')(layer)
+    model = Model(inputs=input_layer, outputs=output)
     model.compile(loss=keras.losses.binary_crossentropy,
                   optimizer=keras.optimizers.Adam(learning_rate), metrics=['accuracy'])
     model.summary()
@@ -106,8 +99,8 @@ def filter_loss(loss, beta):
         else:
             avg_loss[i] = beta*avg_loss[i-1]+(1-beta)*loss[i]
 
-            # compute smoothed loss
-            smoothed_loss[i] = avg_loss[i]/(1-beta**(i+1))
+        # compute smoothed loss
+        smoothed_loss[i] = avg_loss[i]/(1-beta**(i+1))
 
     return smoothed_loss
 
@@ -120,7 +113,8 @@ if __name__ == '__main__':
     if SEARCH is True:
         # import data
         data = import_training_set()
-        # delete first 85 days of dataset
+
+        # delete first 85 days of data
         data = data[data["date"] > 85]
         data["date"] = data["date"]-85
 
@@ -134,14 +128,12 @@ if __name__ == '__main__':
         data = data.drop(redunant_feat, axis=1)
 
         print('Imported successfully')
-        features = [c for c in data.columns if 'feature' in c]
-        cv = TimeSeriesSplit(n_splits=5, gap=10)
 
         # split data into train and test set
         X_train, y_train, X_test, y_test = split_data(data)
 
         # compute
-        input_dim = X_train.shape[1]
+        dimension = X_train.shape[1]
 
         # create two empty arrays that will contain the learning rate and the loss values
         # for each iteration
@@ -149,46 +141,46 @@ if __name__ == '__main__':
         value_loss = np.zeros(40)
 
         # starting learning rate
-        learn_rate = 1e-8 * 1.22**51
+        LEARN_RATE = 1e-8 * 1.22**51
 
         # computational time
         start = time.time()
-        for i in range(40):
+        for j in range(40):
 
             # set hyperparameters
-            hidden_units = [128, 256, 256, 128]
-            dropout_rates = [0.10143786981358652, 0.19720339053599725,
-                             0.2703017847244654, 0.23148340929571917, 0.2357768967777311]
+            hidden = [128, 256, 256, 128]
+            dropout = [0.10143786981358652, 0.19720339053599725,
+                       0.2703017847244654, 0.23148340929571917, 0.2357768967777311]
 
             # build model
             print('Building model...')
-            model = build(data, input_dim, hidden_units, 1, dropout_rates, learn_rate)
+            nn_brain = build(dimension, hidden, 1, dropout, LEARN_RATE)
             print('Training model...')
 
             # fit the model
-            history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
-                                epochs=1, batch_size=8192, verbose=1)
-
-            print(history.history.keys())
+            HISTORY = nn_brain.fit(X_train, y_train, validation_data=(X_test, y_test),
+                                   epochs=1, batch_size=8192, verbose=1)
 
             # save values of loss and learning rate
-            value_loss[i] = (history.history['loss'][0])
-            array_learn[i] = learn_rate
+            value_loss[j] = (HISTORY.history['loss'][0])
+            array_learn[j] = LEARN_RATE
 
             # set learning rate for next cycle
-            learn_rate = learn_rate*1.22
-            print(learn_rate)
+            LEARN_RATE = LEARN_RATE*1.22
+            print(LEARN_RATE)
 
         # save results t a csv file so we can use them later
         np.savetxt("losss.txt", value_loss)
 
     if PLOT is True:
+        # load loss values
         raw_loss = np.loadtxt("losss.txt")
         new_loss = filter_loss(raw_loss, 0.95)
 
+        # create an array of learning rates
         l_rate = np.zeros(39)
-        for i in range(39):
-            l_rate[i] = 1.22**(51+i)*1e-8
+        for k in range(39):
+            l_rate[k] = 1.22**(51+k)*1e-8
 
         new_loss[0] = 0.7559  # i don't know why but it doesn't register first value
 
